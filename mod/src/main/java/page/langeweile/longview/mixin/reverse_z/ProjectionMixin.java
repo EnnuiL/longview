9853724aedcbaa46ea47eedfab764671c3e87c6f
Ennui Langeweile <ennui@langeweile.page>
2026-02-25 15:03:41 -0300

    Remove OpenGL-specific reverse-Z code The only part that isn't platform-agnostic in reverse Z is the shader patching, which I will think about

--- mod/src/main/java/page/langeweile/longview/mixin/reverse_z/ProjectionMixin.java
+++ mod/src/main/java/page/langeweile/longview/mixin/reverse_z/ProjectionMixin.java
@@ -7,30 +7,20 @@ package page.langeweile.longview.mixin.reverse_z;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.renderer.Projection;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import page.langeweile.longview.api.LongviewDevice;
 
 @Mixin(Projection.class)
 public abstract class ProjectionMixin {
 	@WrapOperation(method = "getMatrix", at = @At(value = "INVOKE", target = "Lorg/joml/Matrix4f;setPerspective(FFFFZ)Lorg/joml/Matrix4f;"))
 	private Matrix4f invertPerspectiveMatrixZ(Matrix4f instance, float fovy, float aspect, float zNear, float zFar, boolean zZeroToOne, Operation<Matrix4f> original) {
-		if (((LongviewDevice) RenderSystem.getDevice()).supportsReverseZ()) {
-			return original.call(instance, fovy, aspect, zFar, zNear, zZeroToOne);
-		} else {
-			return original.call(instance, fovy, aspect, zNear, zFar, zZeroToOne);
-		}
+		return original.call(instance, fovy, aspect, zFar, zNear, zZeroToOne);
 	}
 
 	@WrapOperation(method = "getMatrix", at = @At(value = "INVOKE", target = "Lorg/joml/Matrix4f;setOrtho(FFFFFFZ)Lorg/joml/Matrix4f;"))
 	private Matrix4f invertOrthogonalMatrixZ(Matrix4f instance, float left, float right, float bottom, float top, float zNear, float zFar, boolean zZeroToOne, Operation<Matrix4f> original) {
-		if (((LongviewDevice) RenderSystem.getDevice()).supportsReverseZ()) {
-			return original.call(instance, left, right, bottom, top, zFar, zNear, zZeroToOne);
-		} else {
-			return original.call(instance, left, right, bottom, top, zNear, zFar, zZeroToOne);
-		}
+		return original.call(instance, left, right, bottom, top, zFar, zNear, zZeroToOne);
 	}
 }
