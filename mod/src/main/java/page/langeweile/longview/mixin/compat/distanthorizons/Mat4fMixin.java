7ff5af3be0ea5053682cd67be4a819ac19f60dc2
Ennui Langeweile <ennui@langeweile.page>
2026-05-02 20:27:00 -0300

    Add support for Distant Horizons (#9) Because I ain't creating a GitLab account for this, and plus, they did put the effort to migrate to Blaze3D, which I commend them for it. Do not expect any improvements like the ones that Voxy had enjoyed because they are somehow cursed by jaggies anyway (smooth jaggies????)

---
+++ mod/src/main/java/page/langeweile/longview/mixin/compat/distanthorizons/Mat4fMixin.java
@@ -0,0 +1,34 @@
+package page.langeweile.longview.mixin.compat.distanthorizons;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.seibel.distanthorizons.api.objects.math.DhApiMat4f;
+import org.spongepowered.asm.mixin.Mixin;
+import page.langeweile.longview.impl.LongviewImpl;
+
+// Them reimplementing Minecraft 1.16.5's Mat4f class is *horrid*, but we do what we must do.
+@Mixin(targets = "com.seibel.distanthorizons.core.util.math.Mat4f")
+public abstract class Mat4fMixin extends DhApiMat4f {
+	// The "perspective" method is unused, we won't bother with it
+
+	@WrapMethod(method = "setClipPlanes")
+	public void swapClipPlanes(float nearClip, float farClip, Operation<Object> original) {
+		boolean reverseZ = LongviewImpl.isZReversed();
+		boolean zZeroToOne = RenderSystem.getDevice().isZZeroToOne();
+
+		if (reverseZ) {
+			float buf = nearClip;
+			nearClip = farClip;
+			farClip = buf;
+		}
+
+		if (zZeroToOne) {
+			// Used JOML as a reference here
+			this.m22 = farClip / (nearClip - farClip);
+			this.m23 = farClip * nearClip / (nearClip - farClip);
+		} else {
+			original.call(nearClip, farClip);
+		}
+	}
+}
